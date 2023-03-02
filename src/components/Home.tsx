9f7c6a81d1f58921a5c887ca299c7df507be4ac9
NNQA <anhla12h@gmail.com>
2023-03-02 12:46:26 +0700

    feat: add new future Home' channels

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
-import { auth } from '../firebase'
+import { auth, db } from '../firebase'
 import { redirect } from "react-router-dom"
 import ServerIcon from './ServerIcon';
-import { ChevronDoubleDownIcon, ChevronDownIcon, DocumentIcon, PlusIcon } from '@heroicons/react/24/solid';
+import { ChevronDoubleDownIcon, ChevronDownIcon, DocumentIcon, MagnifyingGlassIcon, PlusIcon } from '@heroicons/react/24/solid';
+import Channel from './Channel';
+import { addDoc, collection, doc } from "firebase/firestore";
+import { useCollection } from 'react-firebase-hooks/firestore';
 
 function Home() {
   const [user] = useAuthState(auth);
+  const [channel] = useCollection(collection(db, 'channels'));
+
+  const handleAddChannel = () => {
+    const channelName = prompt("enter a new channel name");
+
+    if (channelName) {
+      addDoc(collection(db, "channels"), {
+        channelName: channelName,
+      })
+    }
+  }
+  const A = () => {
+    console.log("a");
+  }
 
   return <>
     {!user && redirect("/")}
@@ -31,20 +48,43 @@ function Home() {
       </div>
       <div className='bg-discord_channels flex flex-col min-w-max'>
         <h2 className='flex text-white border-b border-gray-800
-          font-bold text-sm py-4 px-6 justify-between
+          font-bold text-sm py-4 pl-9 justify-between 
           items-center hover:bg-discord_serverHover cursor-pointer'>
           QuocAnh(Official)
-          <ChevronDownIcon className='h-5 ml-[60px] ' />
-          </h2>
-          <div className='text-dicord_channelsText 
-            overflow-y-scroll flex-grow scrollbar-thumb-gray-800
-            scrollbar-track-gray-600'
-            >
-            <div>
-              <DocumentIcon className='h-3 mr-3'  />
+          <ChevronDownIcon className='h-5 mr-2' />
+        </h2>
+        <div className='text-dicord_channelsText h-full overflow-hidden'
+        >
+          <div className="h-[calc(100vh-36.8px-24px)] scrollbar
+            scrollbar-thumb-discord_scrollbar scrollbar-track-discord_channels
+            scrollbar-thumb-rounded-2xl scrollbar-w-1 
+            overflow-hidden font-semibold cursor-pointer"
+          >
+            <div className='flex items-center p-2 mb-2'>
+              <DocumentIcon className='h-4 mr-3' />
               <h4>Channels & Roles</h4>
+              <MagnifyingGlassIcon className='h-6 ml-10 hover:text-white' />
+            </div>
+            <div className='flex items-center p-2 mb-2'>
+              <DocumentIcon className='h-4 mr-3' />
+              <h4 className=''>New Channel</h4>
+              <PlusIcon className='h-6 ml-auto hover:text-white'
+                onClick={handleAddChannel} />
+            </div>
+            <div className='flex flex-col space-y-2 px-2 mb-4'>
+              {channel?.docs.map((doc) => (
+                <Channel
+                  key={doc.id}
+                  id={doc.id}
+                  channelName={doc.data().channelName}
+                />
+              ))}
             </div>
           </div>
+        </div>
+      </div>
+      <div className='w-screen bg-slate-700'>
+        {/* <button onClick={handleAddChannel}>asdsadsad</button> */}
       </div>
     </div>
   </>
